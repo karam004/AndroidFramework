697180eaf4337d8dde6d58800b30c5e7ee99dfa7
abhishek-ub <a35@buffalo.edu>
2015-11-20 01:51:22 -0500

    application choice for queuing

--- packages/apps/Settings/src/com/android/settings/notification/QueueSettings.java
+++ packages/apps/Settings/src/com/android/settings/notification/QueueSettings.java
@@ -13,14 +13,22 @@ import android.provider.Settings.System;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import android.preference.PreferenceCategory;
+import android.preference.CheckBoxPreference;
+
+import android.content.pm.PackageManager;
+import android.content.pm.ApplicationInfo;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
+import java.util.List;
+
 public class QueueSettings extends SettingsPreferenceFragment {
 
 	private static final String TAG = "ACSPROJECT";
-
+    private static final String APP_LIST = "applist";
     private final SettingsObserver mSettingsObserver = new SettingsObserver();
 
     private Context mContext;
@@ -30,9 +38,35 @@ public class QueueSettings extends SettingsPreferenceFragment {
         super.onCreate(savedInstanceState);
 
         Log.d(TAG, "QueueSettings on create");
+
         addPreferencesFromResource(R.xml.queuing_setting);
 
         mContext = getActivity();
+
+        final PackageManager pm = mContext.getPackageManager();
+        //get a list of installed apps.
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        // find catagory to add preference
+        PreferenceCategory targetCategory = (PreferenceCategory)findPreference(APP_LIST);
+
+        for (ApplicationInfo packageInfo : packages) {
+            //create one check box for each app
+            CheckBoxPreference checkBoxPreference = new CheckBoxPreference(mContext);
+ 
+            if ((packageInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
+
+                Log.d(TAG, "Installed package :" + packageInfo.packageName);
+                //make sure each key is unique 
+                checkBoxPreference.setKey(packageInfo.packageName);
+                checkBoxPreference.setTitle(packageInfo.packageName);
+                checkBoxPreference.setIcon(packageInfo.icon);
+                checkBoxPreference.setChecked(true);
+
+                targetCategory.addPreference(checkBoxPreference);
+            }
+            
+        }
 
     }
 
